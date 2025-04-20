aac7b23830711ebbe1c4dac28f970212d3aec419
Hemant1234567890h <bittusemare@gmail.com>
2025-04-20 14:00:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, send_file
+import os
+from pdf2docx import Converter
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/convert', methods=['POST'])
+def convert():
+    if 'pdf_file' not in request.files:
+        return "No file uploaded", 400
+
+    pdf_file = request.files['pdf_file']
+    filename = secure_filename(pdf_file.filename)
+    pdf_path = os.path.join(UPLOAD_FOLDER, filename)
+    pdf_file.save(pdf_path)
+
+    docx_filename = filename.rsplit('.', 1)[0] + '.docx'
+    docx_path = os.path.join(UPLOAD_FOLDER, docx_filename)
+
+    cv = Converter(pdf_path)
+    cv.convert(docx_path, start=0, end=None)
+    cv.close()
+
+    return send_file(docx_path, as_attachment=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
